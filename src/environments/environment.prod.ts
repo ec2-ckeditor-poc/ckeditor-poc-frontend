0fab05f84ea82e3d2e83ca1699467ea98808949b
dangersvn <haidang063@gmail.com>
2023-12-04 23:55:00 -0500

    Update Prod endpoint environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const apiURL = "https://15ruwybtx6.execute-api.us-east-1.amazonaws.com/prod/visitorCount"
+export const apiURL = "https://xiifgh8cwj.execute-api.us-east-1.amazonaws.com/prod/visitorCount"
